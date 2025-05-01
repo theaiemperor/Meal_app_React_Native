a745454a000ca6ff7561a1223e983db7128340ae
Arman <119550818+theaiemperor@users.noreply.github.com>
2025-05-01 12:49:52 +0530

    Migrated From Expo 49 to 53
    Expo SDK Upgrade
    Switched from NPM to YARN

--- Screens/FavouriteScreen.tsx
+++ Screens/FavouriteScreen.tsx
@@ -1,52 +1,56 @@
-import {ScrollView, StyleSheet, Text, View} from "react-native";
-import {MEALS} from "../data/data";
-import {useSelector} from "react-redux";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
 import MealItem from "../Components/MealItem";
+import { MEALS } from "../data/data";
 
 const EmptyView = () => {
-    return <View style={styles.emptyContainer}>
-        <Text style={styles.title}>
-            You have no favorite meals yet.
-        </Text>
+  return (
+    <View style={styles.emptyContainer}>
+      <Text style={styles.title}>You have no favorite meals yet.</Text>
     </View>
-}
-
+  );
+};
 
 export default function FavouriteScreen() {
-    const favouriteMealsIds = useSelector((state: { favouriteMeals: { ids: string[] } }) => state.favouriteMeals.ids)
-    const favouriteMeal = MEALS.filter(item => favouriteMealsIds.includes(item.id))
+  const favouriteMealsIds = useSelector(
+    (state: { favouriteMeals: { ids: string[] } }) => state.favouriteMeals.ids
+  );
+  const favouriteMeal = MEALS.filter((item) =>
+    favouriteMealsIds.includes(item.id)
+  );
 
-
-    return <>
-        {
-            favouriteMeal.length === 0 ? <EmptyView/> : <ScrollView>
-                {
-                    favouriteMeal.map(item => <View key={item.id}  style={styles.container}>
-                            <MealItem key={item.id} data={item}/>
-                        </View>
-                    )
-                }
-            </ScrollView>
-        }
+  return (
+    <>
+      {favouriteMeal.length === 0 ? (
+        <EmptyView />
+      ) : (
+        <ScrollView>
+          {favouriteMeal.map((item) => (
+            <View key={item.id} style={styles.container}>
+              <MealItem key={item.id} data={item} />
+            </View>
+          ))}
+        </ScrollView>
+      )}
     </>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    emptyContainer: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    title: {
-        color: 'white',
-        padding: 5,
-        margin: 5,
-        fontSize: 23,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    container: {
-        flex: 1,
-        margin: 20
-    }
-})
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+    color: "white",
+    padding: 5,
+    margin: 5,
+    fontSize: 23,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  container: {
+    flex: 1,
+    margin: 20,
+  },
+});
--- Screens/MealsDetailScreen.tsx
+++ Screens/MealsDetailScreen.tsx
@@ -1,97 +1,98 @@
-import {Image, ScrollView, StyleSheet, Text, View} from "react-native";
-import {MEALS} from "../data/data";
+import { useLayoutEffect } from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import IconButton from "../Components/IconButton";
 import List from "../Components/List";
 import MealDescription from "../Components/MealDescription";
-import {useLayoutEffect} from "react";
-import IconButton from "../Components/IconButton";
-import {useDispatch, useSelector} from "react-redux";
-import {addFavorite, removeFavorite} from "../store/redux/favourites";
+import { MEALS } from "../data/data";
+import { addFavorite, removeFavorite } from "../store/redux/favourites";
 
-export default function MealsDetailScreen({route, navigation}: any) {
-    const mealId = route.params.mealId
-    const meal = MEALS.find(item => item.id === mealId)
+export default function MealsDetailScreen({ route, navigation }: any) {
+  const mealId = route.params.mealId;
+  const meal = MEALS.find((item) => item.id === mealId);
 
-    // favourite meal related stuff
-    const favouriteMealIds = useSelector((state: { favouriteMeals: { ids: string[] } }) => state.favouriteMeals.ids)
-    const dispatch = useDispatch()
-    const mealIsFavourite = favouriteMealIds.includes(mealId)
+  // favourite meal related stuff
+  const favouriteMealIds = useSelector(
+    (state: { favouriteMeals: { ids: string[] } }) => state.favouriteMeals.ids
+  );
+  const dispatch = useDispatch();
+  const mealIsFavourite = favouriteMealIds.includes(mealId);
 
-
-    const favouriteButtonHandler = () => {
-        if (mealIsFavourite) {
-            dispatch(removeFavorite({id: mealId}))
-        } else {
-            dispatch(addFavorite({id: mealId}))
-        }
+  const favouriteButtonHandler = () => {
+    if (mealIsFavourite) {
+      dispatch(removeFavorite({ id: mealId }));
+    } else {
+      dispatch(addFavorite({ id: mealId }));
     }
+  };
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight: () => {
-                return (
-                    <IconButton color='white'
-                                icon={mealIsFavourite ? 'star' : 'star-outline'}
-                                onPress={favouriteButtonHandler}
-                    />
-                )
-            }
-        });
-    }, [favouriteButtonHandler, navigation])
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            color="white"
+            icon={mealIsFavourite ? "star" : "star-outline"}
+            onPress={favouriteButtonHandler}
+          />
+        );
+      },
+    });
+  }, [favouriteButtonHandler, navigation]);
 
-    return (
-        <View>
-            {
-                meal &&
-                <>
-                    <ScrollView style={styles.container}>
-                        <View style={styles.mainContainer}>
-                            <View style={styles.imageContainer}>
-                                <Image source={{uri: meal.imageUrl}} style={styles.image}/>
-                            </View>
-                            <Text style={styles.title}>{meal.title}</Text>
-                            <MealDescription complexity={meal.complexity}
-                                             duration={meal.duration}
-                                             affordability={meal.affordability}/>
-                        </View>
-                        <View>
-                            <List title="INGREDIENTS" data={meal.ingredients}/>
-                        </View>
-                        <View>
-                            <List title="STEPS" data={meal.steps}/>
-                        </View>
-                    </ScrollView>
-                </>
-            }
-        </View>
-    )
+  return (
+    <View>
+      {meal && (
+        <ScrollView style={styles.container}>
+          <View style={styles.mainContainer}>
+            <View style={styles.imageContainer}>
+              <Image source={{ uri: meal.imageUrl }} style={styles.image} />
+            </View>
+            <Text style={styles.title}>{meal.title}</Text>
+            <MealDescription
+              complexity={meal.complexity}
+              duration={meal.duration}
+              affordability={meal.affordability}
+            />
+          </View>
+          <View>
+            <List title="INGREDIENTS" data={meal.ingredients} />
+          </View>
+          <View>
+            <List title="STEPS" data={meal.steps} />
+          </View>
+        </ScrollView>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 2,
-        marginBottom: 5,
-    },
-    mainContainer: {
-        borderStyle: 'solid',
-        borderWidth: 3,
-        marginTop: 5,
-        borderRadius: 10,
-        borderColor: '#180b0b',
-        backgroundColor: '#180b0b'
-    },
-    imageContainer: {
-        marginBottom: 20,
-        alignItems: "center",
-    },
-    image: {
-        width: "90%",
-        height: 200,
-        margin: 5
-    },
-    title: {
-        color: "white",
-        fontSize: 20,
-        textAlign: 'center',
-        fontWeight: "bold",
-    }
-})
+  container: {
+    padding: 2,
+    marginBottom: 5,
+  },
+  mainContainer: {
+    borderStyle: "solid",
+    borderWidth: 3,
+    marginTop: 5,
+    borderRadius: 10,
+    borderColor: "#180b0b",
+    backgroundColor: "#180b0b",
+  },
+  imageContainer: {
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  image: {
+    width: "90%",
+    height: 200,
+    margin: 5,
+  },
+  title: {
+    color: "white",
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
